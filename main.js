67a0b5b706d54a6b80e16e47d7cf1da078212024
Mohammad Chughtai <mohammad.hunan@Mohammads-MacBook-Pro.local>
2021-01-21 15:10:28 -0500

    Add writes from both processes

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+const Realm = require("realm");
+const BSON = require("BSON");
+
+
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
+  // create an electron browser window 
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -8,7 +13,50 @@ function createWindow () {
       nodeIntegration: true
     }
   })
+
+  // create an ongoing process (by reading from stdin) to prevent the Sync Connection from ending prematurely
+  process.stdin.resume();
+
   win.loadFile('index.html')
 }
 
-app.whenReady().then(()=>createWindow());
+app.whenReady().then(async () => {
+    const app = new Realm.App({ id: "electron-tester-qokcs" }); // create a new instance of the Realm.App
+
+    await app.logIn(new Realm.Credentials.anonymous());
+    const DogSchema = {
+        name: "Dog",
+        properties: {
+          _id: 'objectId',
+          name: "string",
+          age: "int",
+        },
+        primaryKey: '_id'
+    };
+  
+    const config = {
+        schema: [DogSchema],
+        path: "my.realm",
+        sync: {
+          user: app.currentUser,
+          partitionValue: "myPartition"
+        }
+      };
+    
+      // open a synced realm
+      const realm = await Realm.open(config);
+
+      const dogs = realm.objects("Dog");
+      console.log(`Renderer: Number of Dog objects: ${dogs.length}`);
+    
+      realm.write(() => {
+        realm.create("Dog", {
+            _id: new BSON.ObjectID(),
+            name: "Fizzbuzz2",
+            age: 5
+        })
+      })
+
+    createWindow();
+    
+})
---
+++ renderer.js
@@ -0,0 +1,46 @@
+const Realm = require("realm");
+const BSON = require("BSON");
+const app = new Realm.App({ id: "electron-tester-qokcs" }); // create a new instance of the Realm.App
+
+async function run() {
+
+  // login with an anonymous credential
+  await app.logIn(new Realm.Credentials.anonymous());
+
+  const DogSchema = {
+      name: "Dog",
+      properties: {
+        _id: 'objectId',
+        name: "string",
+        age: "int",
+      },
+      primaryKey: '_id'
+  };
+
+
+const config = {
+    path: "my.realm",
+    schema: [DogSchema],
+    sync: true,
+  };
+
+
+  const realm = new Realm(config);
+  
+  const dogs = realm.objects("Dog");
+  console.log(`Renderer: Number of Dog objects: ${dogs.length}`);
+
+
+
+  realm.write(() => {
+    realm.create("Dog", {
+        _id: new BSON.ObjectID(),
+        name: "Fizzbuzz",
+        age: 2
+    })
+  })
+
+}
+run().catch(err => {
+  console.error("Failed to open realm:", err)
+});
