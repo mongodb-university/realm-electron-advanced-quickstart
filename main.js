3a0d99194677965d6d56d2973c8f65c88595567e
Mohammad Chughtai <mohammad.hunan@Mohammads-MacBook-Pro.local>
2021-01-21 13:11:15 -0500

    initial setup with eslint, prettier + added readme

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const a = 4;
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(()=>createWindow());
